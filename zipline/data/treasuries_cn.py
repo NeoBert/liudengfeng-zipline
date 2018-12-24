fc491646fe97317d3593381e476fd74bf9b214d7
ldf <liu.dengfeng@hotmail.com>
2018-12-24 23:39:52 +0800

    本地国库券利率数据

--- zipline/data/treasuries_cn.py
+++ zipline/data/treasuries_cn.py
@@ -4,9 +4,9 @@
 import pandas as pd
 
 from cnswd.utils import sanitize_dates
-from cnswd.sql.cn import Treasury
+from cnswd.sql.szsh import Treasury
 from cnswd.sql.base import session_scope
-
+from cnswd.websource.treasuries import EARLIEST_POSSIBLE_DATE
 TREASURY_COL_NAMES = ['date', '1month', '3month', '6month', '1year', '2year', '3year',
                       '5year', '7year', '10year', '20year', '30year']
 
@@ -15,7 +15,7 @@ def earliest_possible_date():
     """
     The earliest date for which we can load data from this module.
     """
-    return pd.Timestamp('2006-3-1', tz='UTC').normalize()
+    return EARLIEST_POSSIBLE_DATE
 
 
 def get_treasury_data(start_date, end_date):
@@ -23,14 +23,13 @@ def get_treasury_data(start_date, end_date):
     # 确保为date类型
     start_date = pd.Timestamp(start_date).date()
     end_date = pd.Timestamp(end_date).date()
-    with session_scope('cn') as sess:
+    with session_scope('szsh') as sess:
         query = sess.query(
             Treasury.date,
             Treasury.m1,
             Treasury.m3,
             Treasury.m6,
             Treasury.y1,
-            Treasury.y2,
             Treasury.y3,
             Treasury.y5,
             Treasury.y7,
@@ -39,10 +38,10 @@ def get_treasury_data(start_date, end_date):
             Treasury.y30
         ).filter(Treasury.date.between(start_date, end_date))
         df = pd.DataFrame.from_records(query.all())
+        # 缺少2年数据，使用简单平均插值
+        value = (df.iloc[:,4] + df.iloc[:, 5]) / 2
+        df.insert(5,'2year',value)
         df.columns = TREASURY_COL_NAMES
         df.set_index(keys='date', inplace=True)
         df.index = pd.DatetimeIndex(df.index)
-        # 缺少2/7年数据，使用简单平均插值
-        df['2year'] = (df['1year'] + df['3year']) / 2
-        df['7year'] = (df['5year'] + df['10year']) / 2
     return df.tz_localize('UTC')
