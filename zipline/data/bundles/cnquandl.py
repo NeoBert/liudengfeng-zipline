09e9e9cd469e60d19e202c7101ad843f726dcee9
ldf <liu.dengfeng@hotmail.com>
2018-12-24 22:50:01 +0800

    cn数据包

---
+++ zipline/data/bundles/cnquandl.py
@@ -0,0 +1,355 @@
+"""
+构造股票日线数据集
+
+备注：
+    1. 如使用用int(stock_code)代表sid，必须在写入资产元数据时，提供sid列
+    2. 默认只写入A股，且在市股票数据
+
+TODO:
+    1. 在`cnswd`包完成退市及B股数据日线数据下载功能
+    2. 上述二部分需要`selenium`才能在网易网完成下载
+"""
+
+import pandas as pd
+from logbook import Logger
+
+from . import core as bundles
+# from ..constants import ADJUST_FACTOR, TEST_SYMBOLS, OHLC
+# from ..sqldata import (fetch_single_equity, fetch_single_quity_adjustments,
+#                        gen_asset_metadata, fetch_single_minutely_equity)
+
+TODAY = pd.Timestamp('today').normalize()
+log = Logger('cnquandl')
+
+
+def _exchanges():
+    # 通过 `股票.exchange = exchanges.exchange`来关联
+    return pd.DataFrame(
+        {
+            'exchange': ['深圳证券交易所', '上海证券交易所'],
+            'canonical_name': ['XSHE', 'XSHG'],
+            'country_code': ['CN', 'CN']
+        }
+    )
+
+
+def _to_sid(x):
+    """符号转换为sid"""
+    return int(x)
+
+
+def _to_symbol(x):
+    """sid转换为符号"""
+    return str(x).zfill(6)
+
+
+def _adjusted_raw_data(raw_df):
+    """调整原始数据单位，转换适用于unit32类型的数值"""
+    data = raw_df.copy()
+    # 增强兼容性
+    for col in data.columns:
+        if col in ADJUST_FACTOR.keys():
+            adj = ADJUST_FACTOR.get(col, 1)
+            data.loc[:, col] = data[col] * adj
+    return data
+
+
+def _update_splits(splits, asset_id, origin_data):
+    if origin_data.empty:
+        # 如为空表，直接返回，不进行任何处理
+        return
+    ratio = origin_data['s_ratio'] + origin_data['z_ratio']
+    # 调整适应于zipline算法
+    # date -> datetime64[ns]
+    df = pd.DataFrame({'ratio': 1 / (1 + ratio),
+                       'effective_date': pd.to_datetime(origin_data.ex_date),
+                       'sid': asset_id})
+    # df['ratio'] = df.ratio.astype('float')
+    splits.append(df)
+
+
+def _update_dividends(dividends, asset_id, origin_data):
+    if origin_data.empty:
+        return
+    # date -> datetime64[ns]
+    df = pd.DataFrame({'record_date': pd.to_datetime(origin_data['record_date']),
+                       'ex_date': pd.to_datetime(origin_data['ex_date']),
+                       'declared_date': pd.to_datetime(origin_data['declared_date']),
+                       'pay_date': pd.to_datetime(origin_data['pay_date']),
+                       'amount': origin_data['amount'],
+                       'sid': asset_id})
+    dividends.append(df)
+
+
+def gen_symbol_data(symbol_map,
+                    sessions,
+                    splits,
+                    dividends,
+                    is_minutely):
+    for _, symbol in symbol_map.iteritems():
+        asset_id = _to_sid(symbol)
+        if not is_minutely:
+            # 日线原始数据，只需要OHLCV列
+            raw_data = fetch_single_equity(
+                symbol,
+                start=sessions[0],
+                end=sessions[-1],
+            )
+            # 不得包含涨跌幅列，含有正负号
+            raw_data.drop('change_pct', axis=1, inplace=True)
+            # 百分比调整为小数
+            raw_data['turnover'] = raw_data.turnover / 100.
+            # 调整数据精度
+            raw_data = _adjusted_raw_data(raw_data)
+            # 以日期、符号为索引
+            raw_data.set_index(['date', 'symbol'], inplace=True)
+            # 时区调整，以0.0填充na
+            # 转换为以日期为索引的表(与sessions保持一致)
+            asset_data = raw_data.xs(
+                symbol,
+                level=1
+            ).reindex(
+                sessions.tz_localize(None)
+            ).fillna(0.0)
+        else:
+            # 处理分钟级别数据
+            asset_data = fetch_single_minutely_equity(
+                symbol,
+                start=sessions[0],
+                end=sessions[-1],
+            ).tz_localize('Asia/Shanghai').tz_convert('utc')
+
+        # 顺带处理分红派息
+        # 获取原始调整数据
+        raw_adjustment = fetch_single_quity_adjustments(symbol,
+                                                        start=sessions[0],
+                                                        end=sessions[-1])
+        # 当非空时才执行
+        if not raw_adjustment.empty:
+            # 剔除未来事件
+            raw_adjustment = raw_adjustment[raw_adjustment.ex_date <= TODAY]
+            # 更新送转
+            # 送转比率大于0才有意义
+            ratio = raw_adjustment.s_ratio + raw_adjustment.z_ratio
+            raw_splits = raw_adjustment.loc[ratio > 0.0, :]
+            _update_splits(splits, asset_id, raw_splits)
+
+            # 更新股利
+            raw_dividends = raw_adjustment.loc[raw_adjustment.amount > 0.0, :]
+            _update_dividends(dividends, asset_id, raw_dividends)
+
+        yield asset_id, asset_data
+
+
+@bundles.register('cndaily',
+                  calendar_name='XSHE',
+                  #   end_session=pd.Timestamp('now', tz='Asia/Shanghai'),
+                  minutes_per_day=241)
+def cndaily_bundle(environ,
+                   asset_db_writer,
+                   minute_bar_writer,
+                   daily_bar_writer,
+                   adjustment_writer,
+                   calendar,
+                   start_session,
+                   end_session,
+                   cache,
+                   show_progress,
+                   output_dir):
+    """Build a zipline data bundle from the cnstock dataset.
+    """
+    log.info('读取股票元数据......')
+    metadata = gen_asset_metadata(False)
+    # 资产元数据写法要求添加sid列！！！
+    metadata['sid'] = metadata.symbol.map(_to_sid)
+    symbol_map = metadata.symbol
+    sessions = calendar.sessions_in_range(start_session, end_session)
+
+    log.info('日线数据集（股票数量：{}）'.format(len(symbol_map)))
+
+    # 写入股票元数据
+    if show_progress:
+        log.info('写入资产元数据')
+    asset_db_writer.write(metadata, exchanges=_exchanges())
+
+    splits = []
+    dividends = []
+    daily_bar_writer.write(
+        gen_symbol_data(
+            symbol_map,
+            sessions,
+            splits,
+            dividends,
+            is_minutely=False
+        ),
+        show_progress=show_progress,
+    )
+
+    adjustment_writer.write(
+        splits=None if len(splits) == 0 else pd.concat(
+            splits, ignore_index=True),
+        dividends=None if len(dividends) == 0 else pd.concat(
+            dividends, ignore_index=True),
+    )
+
+
+# 不可包含退市或者暂停上市的股票代码
+
+
+@bundles.register('cnminutely',
+                  calendar_name='XSHE',
+                  start_session=pd.Timestamp(
+                      'now', tz='Asia/Shanghai') - pd.Timedelta(days=30),
+                  #   end_session=pd.Timestamp('now', tz='Asia/Shanghai'),
+                  minutes_per_day=241)
+def cnminutely_bundle(environ,
+                      asset_db_writer,
+                      minute_bar_writer,
+                      daily_bar_writer,
+                      adjustment_writer,
+                      calendar,
+                      start_session,
+                      end_session,
+                      cache,
+                      show_progress,
+                      output_dir):
+    """Build a zipline data bundle from the cnstock dataset.
+    """
+    log.info('读取股票元数据......')
+    metadata = gen_asset_metadata()
+    metadata['sid'] = metadata.symbol.map(_to_sid)
+    symbol_map = metadata.symbol
+    sessions = calendar.sessions_in_range(start_session, end_session)
+
+    log.info('分钟级别数据集（股票数量：{}）'.format(len(symbol_map)))
+
+    # 写入股票元数据
+    if show_progress:
+        log.info('写入资产元数据')
+    asset_db_writer.write(metadata, exchanges=_exchanges())
+
+    splits = []
+    dividends = []
+    minute_bar_writer.write(
+        gen_symbol_data(
+            symbol_map,
+            sessions,
+            splits,
+            dividends,
+            is_minutely=True
+        ),
+        show_progress=show_progress,
+    )
+
+    adjustment_writer.write(
+        splits=None if len(splits) == 0 else pd.concat(
+            splits, ignore_index=True),
+        dividends=None if len(dividends) == 0 else pd.concat(
+            dividends, ignore_index=True),
+    )
+
+
+# # 测试数据集
+
+# 不可包含退市或者暂停上市的股票代码
+
+@bundles.register('cdtest',
+                  calendar_name='XSHE',
+                  #   end_session=pd.Timestamp('now', tz='Asia/Shanghai'),
+                  minutes_per_day=241)
+def cntdaily_bundle(environ,
+                    asset_db_writer,
+                    minute_bar_writer,
+                    daily_bar_writer,
+                    adjustment_writer,
+                    calendar,
+                    start_session,
+                    end_session,
+                    cache,
+                    show_progress,
+                    output_dir):
+    """Build a zipline test data bundle from the cnstock dataset.
+    """
+    log.info('读取股票元数据(测试集)......')
+    # 测试集
+    metadata = gen_asset_metadata(False)
+    metadata = metadata[metadata.symbol.isin(TEST_SYMBOLS)]
+    metadata.reset_index(inplace=True, drop=True)
+    metadata['sid'] = metadata.symbol.map(_to_sid)
+    sessions = calendar.sessions_in_range(start_session, end_session)
+
+    symbol_map = metadata.symbol
+    log.info('生成测试数据集（共{}只）'.format(len(symbol_map)))
+    splits = []
+    dividends = []
+
+    asset_db_writer.write(metadata, exchanges=_exchanges())
+
+    daily_bar_writer.write(
+        gen_symbol_data(
+            symbol_map,
+            sessions,
+            splits,
+            dividends,
+            is_minutely=False,
+        ),
+        show_progress=show_progress,
+    )
+    adjustment_writer.write(
+        splits=pd.concat(splits, ignore_index=True) if len(splits) else None,
+        dividends=pd.concat(dividends, ignore_index=True) if len(
+            dividends) else None,
+    )
+
+
+@bundles.register('cmtest',
+                  calendar_name='XSHE',
+                  start_session=pd.Timestamp(
+                      'now', tz='Asia/Shanghai') - pd.Timedelta(days=30),
+                  #   end_session=pd.Timestamp('now', tz='Asia/Shanghai'),
+                  minutes_per_day=241)
+def cntminutely_bundle(environ,
+                       asset_db_writer,
+                       minute_bar_writer,
+                       daily_bar_writer,
+                       adjustment_writer,
+                       calendar,
+                       start_session,
+                       end_session,
+                       cache,
+                       show_progress,
+                       output_dir):
+    """Build a zipline test data bundle from the cnstock dataset.
+    """
+    log.info('读取股票元数据(测试集)......')
+    # 测试集
+    metadata = gen_asset_metadata()
+    metadata = metadata[metadata.symbol.isin(TEST_SYMBOLS)]
+    metadata.reset_index(inplace=True, drop=True)
+    metadata['sid'] = metadata.symbol.map(_to_sid)
+    sessions = calendar.sessions_in_range(start_session, end_session)
+
+    symbol_map = metadata.symbol
+    log.info('生成测试数据集（共{}只）'.format(len(symbol_map)))
+    splits = []
+    dividends = []
+
+    asset_db_writer.write(metadata, exchanges=_exchanges())
+
+    minute_bar_writer.write(
+        gen_symbol_data(
+            symbol_map,
+            sessions,
+            splits,
+            dividends,
+            is_minutely=True
+        ),
+        show_progress=show_progress,
+    )
+
+    adjustment_writer.write(
+        splits=None if len(splits) == 0 else pd.concat(
+            splits, ignore_index=True),
+        dividends=None if len(dividends) == 0 else pd.concat(
+            dividends, ignore_index=True),
+    )
---
+++ zipline/data/sqldata.py
@@ -0,0 +1,390 @@
+"""
+
+本地数据查询及预处理，适用于zipline ingest写入
+
+"""
+
+from sqlalchemy import func
+import pandas as pd
+import numpy as np
+from cnswd.sql.base import session_scope
+from cnswd.sql.cn import StockDaily, CJMX
+from cnswd.sql.szx import StockInfo, Dividend
+
+
+DAILY_COLS = ['symbol', 'date',
+              'open', 'high', 'low', 'close',
+              'prev_close', 'change_pct',
+              'volume', 'amount', 'turnover', 'cmv', 'tmv']
+OHLCV_COLS = ['open', 'high', 'low', 'close', 'volume']
+MINUTELY_COLS = ['symbol', 'date'] + OHLCV_COLS
+
+ADJUSTMENT_COLS = ['symbol', 'date', 's_ratio', 'z_ratio', 'amount',
+                   'declared_date', 'record_date', 'ex_date', 'pay_date']
+
+
+def get_exchange(code):
+    """股票所在交易所编码"""
+    # https://www.iso20022.org/10383/iso-10383-market-identifier-codes
+    if code[0] in ('0', '2', '3'):
+        return "深圳证券交易所"
+    elif code[0] in ('6', '9'):
+        return "上海证券交易所"
+    else:
+        return 'unknown'.upper()
+
+
+def _stock_basic_info():
+    """股票上市基础信息"""
+    col_names = ['symbol', 'start_date', 'end_date', 'status', 'stock_type']
+    with session_scope('szx') as sess:
+        query = sess.query(
+            StockInfo.股票代码,
+            StockInfo.上市日期,
+            StockInfo.摘牌日期,
+            StockInfo.上市状态,
+            StockInfo.股票类别,
+        )
+        df = pd.DataFrame.from_records(query.all())
+        df.columns = col_names
+        return df
+
+
+def get_latest_short_name():
+    """
+    获取股票最新股票简称
+
+    Examples
+    --------
+    >>> df = get_end_dates()
+    >>> df.head()
+         symbol  asset_name
+    0     000001    平安银行
+    1     000002    万 科Ａ
+    2     000003   PT金田Ａ
+    3     000004    国农科技
+    4     000005    世纪星源     
+    """
+    col_names = ['symbol', 'last_date', 'asset_name']
+    with session_scope('cn') as sess:
+        query = sess.query(
+            StockDaily.股票代码,
+            func.max(StockDaily.日期),
+            StockDaily.名称
+        ).group_by(
+            StockDaily.股票代码
+        )
+        df = pd.DataFrame.from_records(query.all())
+        df.columns = col_names
+        return df.iloc[:, [0, 2]]
+
+
+def _stock_first_and_last():
+    """自股票日线交易数据查询开始交易及结束交易日期"""
+    col_names = ['symbol', 'first_traded', 'last_traded']
+    with session_scope('cn') as sess:
+        query = sess.query(
+            StockDaily.股票代码,
+            func.min(StockDaily.日期),
+            func.max(StockDaily.日期)
+        ).group_by(
+            StockDaily.股票代码
+        )
+        df = pd.DataFrame.from_records(query.all())
+        df.columns = col_names
+        return df
+
+
+def gen_asset_metadata(only_in=True, only_A=True):
+    """
+    生成股票元数据
+
+    Paras
+    -----
+    only_in : bool
+        是否仅仅包含当前在市的股票，默认为真。
+    only_A : bool
+        是否仅仅为A股股票(即：不包含B股股票)，默认为不包含。
+
+    Examples
+    --------
+    >>> df = gen_asset_metadata()
+    >>> df.head()
+    symbol start_date   end_date       ...       last_traded exchange auto_close_date
+    0  000001 1991-04-03 2018-10-09       ...        2018-10-09     XSHE      2018-10-10
+    1  000002 1991-01-29 2018-10-09       ...        2018-10-09     XSHE      2018-10-10
+    2  000004 1991-01-14 2018-10-09       ...        2018-10-09     XSHE      2018-10-10
+    3  000005 1990-12-10 2018-10-09       ...        2018-10-09     XSHE      2018-10-10
+    4  000006 1992-04-27 2018-10-09       ...        2018-10-09     XSHE      2018-10-10
+    """
+    f_and_l = _stock_first_and_last()
+    latest_name = get_latest_short_name()
+    s_and_e = _stock_basic_info()
+    df = s_and_e.merge(
+        latest_name, 'left', on='symbol'
+    ).merge(
+        f_and_l, 'left', on='symbol'
+    )
+    # 剔除已经退市
+    if only_in:
+        df = df[df.status != '已经退市']
+    del df['status']
+    # 剔除非A股部分
+    if only_A:
+        df = df[df.stock_type == 'A股']
+    del df['stock_type']
+    # 对于未退市的结束日期，以最后交易日期代替
+    df.loc[df.end_date.isna(), 'end_date'] = df.loc[df.end_date.isna(),
+                                                    'last_traded']
+    df.sort_values('symbol', inplace=True)
+    df.reset_index(inplace=True, drop=True)
+    df['exchange'] = df['symbol'].map(get_exchange)
+    df['auto_close_date'] = df['last_traded'].map(
+        lambda x: x + pd.Timedelta(days=1))
+    return df
+
+
+def _fill_zero(df):
+    """填充因为停牌ohlc可能存在的0值"""
+    # 将close放在第一列
+    ohlc_cols = ['close', 'open', 'high', 'low']
+    ohlc = df[ohlc_cols].copy()
+    ohlc.replace(0.0, np.nan, inplace=True)
+    ohlc.close.fillna(method='ffill', inplace=True)
+    # 按列填充
+    ohlc.fillna(method='ffill', axis=1, inplace=True)
+    for col in ohlc_cols:
+        df[col] = ohlc[col]
+    return df
+
+
+def fetch_single_equity(stock_code, start, end):
+    """
+    从本地数据库读取股票期间日线交易数据
+
+    注
+    --
+    1. 除OHLCV外，还包括涨跌幅、成交额、换手率、流通市值、总市值、流通股本、总股本
+    2. 使用bcolz格式写入时，由于涨跌幅存在负数，必须剔除该列！！！
+
+    Parameters
+    ----------
+    stock_code : str
+        要获取数据的股票代码
+    start_date : datetime-like
+        自开始日期(包含该日)
+    end_date : datetime-like
+        至结束日期
+
+    return
+    ----------
+    DataFrame: OHLCV列的DataFrame对象。
+
+    Examples
+    --------
+    >>> symbol = '000333'
+    >>> start_date = '2017-4-1'
+    >>> end_date = pd.Timestamp('2018-4-16')
+    >>> df = fetch_single_equity(symbol, start_date, end_date)
+    >>> df.iloc[:,:8]
+        symbol        date   open   high    low  close  prev_close  change_pct
+    0  000333  2018-04-02  53.30  55.00  52.68  52.84       54.53     -3.0992
+    1  000333  2018-04-03  52.69  53.63  52.18  52.52       52.84     -0.6056
+    2  000333  2018-04-04  52.82  54.10  52.06  53.01       52.52      0.9330
+    3  000333  2018-04-09  52.91  53.31  51.00  51.30       53.01     -3.2258
+    4  000333  2018-04-10  51.45  52.80  51.18  52.77       51.30      2.8655
+    5  000333  2018-04-11  52.78  53.63  52.41  52.98       52.77      0.3980
+    6  000333  2018-04-12  52.91  52.94  51.84  51.87       52.98     -2.0951
+    7  000333  2018-04-13  52.40  52.47  51.01  51.32       51.87     -1.0603
+    8  000333  2018-04-16  51.31  51.80  49.15  49.79       51.32     -2.9813    
+    """
+    start = pd.Timestamp(start).date()
+    end = pd.Timestamp(end).date()
+    with session_scope('cn') as sess:
+        query = sess.query(
+            StockDaily.股票代码,
+            StockDaily.日期,
+            StockDaily.开盘价,
+            StockDaily.最高价,
+            StockDaily.最低价,
+            StockDaily.收盘价,
+            StockDaily.前收盘,
+            StockDaily.涨跌幅,
+            StockDaily.成交量,
+            StockDaily.成交金额,
+            StockDaily.换手率,
+            StockDaily.流通市值,
+            StockDaily.总市值
+        ).filter(
+            StockDaily.股票代码 == stock_code,
+            StockDaily.日期.between(start, end)
+        )
+        df = pd.DataFrame.from_records(query.all())
+        if df.empty:
+            return pd.DataFrame(columns=DAILY_COLS+['circulating_share', 'total_share'])
+        df.columns = DAILY_COLS
+        df = _fill_zero(df)
+        df['circulating_share'] = df.cmv / df.close
+        df['total_share'] = df.tmv / df.close
+        return df
+
+
+def _handle_minutely_data(df, exclude_lunch):
+    """
+    完成单个日期股票分钟级别数据处理
+    """
+    ohlcv = pd.Series(data=df['price'].values,
+                      index=df.datetime).resample('T').ohlc()
+    ohlcv.fillna(method='ffill', inplace=True)
+    # 成交量原始数据单位为手，换为股
+    volumes = pd.Series(data=df['volume'].values,
+                        index=df.datetime).resample('T').sum() * 100
+    ohlcv.insert(4, 'volume', volumes)
+    if exclude_lunch:
+        # 默认包含上下界
+        # 与交易日历保持一致，自31分开始
+        pre = ohlcv.between_time('9:25', '9:31')
+
+        def key(x): return x.date()
+        grouped = pre.groupby(key)
+        opens = grouped['open'].first()
+        highs = grouped['high'].max()
+        lows = grouped['low'].min()  # 考虑是否存在零值？
+        closes = grouped['close'].last()
+        volumes = grouped['volume'].sum()
+        index = pd.to_datetime([str(x) + ' 9:31' for x in opens.index])
+        add = pd.DataFrame({'open': opens.values,
+                            'high': highs.values,
+                            'low': lows.values,
+                            'close': closes.values,
+                            'volume': volumes.values
+                            },
+                           index=index)
+        am = ohlcv.between_time('9:32', '11:30')
+        pm = ohlcv.between_time('13:00', '15:00')
+        return pd.concat([add, am, pm])
+    else:
+        return ohlcv
+
+
+def fetch_single_minutely_equity(stock_code, start, end, exclude_lunch=True):
+    """
+    从本地数据库读取单个股票期间分钟级别交易明细数据
+
+    **注意** 性能原因，超过一定周期的数据，转移至备份数据库。只能查询到近期数据。
+
+    注
+    --
+    1. 仅包含OHLCV列
+    2. 原始数据按分钟进行汇总，first(open),last(close),max(high),min(low),sum(volume)
+
+    Parameters
+    ----------
+    stock_code : str
+        要获取数据的股票代码
+    start_date : datetime-like
+        自开始日期(包含该日)
+    end_date : datetime-like
+        至结束日期
+    exclude_lunch ： bool
+        是否排除午休时间，默认”是“
+
+    return
+    ----------
+    DataFrame: OHLCV列的DataFrame对象。
+
+    Examples
+    --------
+    >>> symbol = '000333'
+    >>> start_date = '2018-4-1'
+    >>> end_date = pd.Timestamp('2018-4-19')
+    >>> df = fetch_single_minutely_equity(symbol, start_date, end_date)
+    >>> df.tail()
+                        close   high    low   open  volume
+    2018-04-19 14:56:00  51.55  51.56  51.50  51.55  376400
+    2018-04-19 14:57:00  51.55  51.55  51.55  51.55   20000
+    2018-04-19 14:58:00  51.55  51.55  51.55  51.55       0
+    2018-04-19 14:59:00  51.55  51.55  51.55  51.55       0
+    2018-04-19 15:00:00  51.57  51.57  51.57  51.57  353900
+    """
+    col_names = ['symbol', 'datetime', 'price', 'volume']
+    start = pd.Timestamp(start).date()
+    end = pd.Timestamp(end).date()
+    with session_scope('cn') as sess:
+        query = sess.query(
+            CJMX.股票代码,
+            CJMX.成交时间,
+            CJMX.成交价,
+            CJMX.成交量,
+        ).filter(
+            CJMX.股票代码 == stock_code,
+            CJMX.成交时间.between(start, end)
+        )
+        df = pd.DataFrame.from_records(query.all())
+        if df.empty:
+            return pd.DataFrame(columns=OHLCV_COLS)
+        df.columns = col_names
+        return _handle_minutely_data(df, exclude_lunch)
+
+
+def fetch_single_quity_adjustments(stock_code, start, end):
+    """
+    从本地数据库读取股票期间分红派息数据
+
+    Parameters
+    ----------
+    stock_code : str
+        要获取数据的股票代码
+    start : datetime-like
+        自开始日期
+    end : datetime-like
+        至结束日期
+
+    return
+    ----------
+    DataFrame对象
+
+    Examples
+    --------
+    >>> fetch_single_quity_adjustments('600000', '2010-4-1', '2018-4-16')
+        symbol        date  amount  ratio record_date    pay_date listing_date
+    0  600000  2010-06-10   0.150    0.3  2010-06-09  2010-06-11   2010-06-10
+    1  600000  2011-06-03   0.160    0.3  2011-06-02  2011-06-07   2011-06-03
+    2  600000  2012-06-26   0.300    0.0  2012-06-25  2012-06-26   2012-06-26
+    3  600000  2013-06-03   0.550    0.0  2013-05-31  2013-06-03   2013-06-03
+    4  600000  2014-06-24   0.660    0.0  2014-06-23  2014-06-24   2014-06-24
+    5  600000  2015-06-23   0.757    0.0  2015-06-19  2015-06-23   2015-06-23
+    6  600000  2016-06-23   0.515    0.1  2016-06-22  2016-06-24   2016-06-23
+    7  600000  2017-05-25   0.200    0.3  2017-05-24  2017-05-26   2017-05-25
+    """
+    start = pd.Timestamp(start).date()
+    end = pd.Timestamp(end).date()
+    with session_scope('szx') as sess:
+        query = sess.query(
+            Dividend.股票代码,
+            Dividend.分红年度,
+            Dividend.送股比例,
+            Dividend.转增比例,
+            Dividend.派息比例人民币,
+            Dividend.股东大会预案公告日期,
+            Dividend.A股股权登记日,
+            Dividend.A股除权日,
+            Dividend.派息日A,
+        ).filter(
+            Dividend.股票代码 == stock_code,
+            Dividend.分红年度.between(start, end)
+        )
+        df = pd.DataFrame.from_records(query.all())
+        if df.empty:
+            # 返回一个空表
+            return pd.DataFrame(columns=ADJUSTMENT_COLS)
+        df.columns = ADJUSTMENT_COLS
+        # nan以0代替
+        df['s_ratio'].fillna(value=0.0, inplace=True)
+        df['z_ratio'].fillna(value=0.0, inplace=True)
+        df['amount'].fillna(value=0.0, inplace=True)
+        # 调整为每股比例
+        df['s_ratio'] = df['s_ratio'] / 10.0
+        df['z_ratio'] = df['z_ratio'] / 10.0
+        df['amount'] = df['amount'] / 10.0
+        return df
