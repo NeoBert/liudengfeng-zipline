ff766b2148f867583702c530dbccd359cf68b223
ldf <liu.dengfeng@hotmail.com>
2019-04-20 18:12:07 +0800

    commit

--- zipline/research/__init__.py
+++ zipline/research/__init__.py
@@ -3,7 +3,7 @@ from .pipebench import run_pipeline
 from .utils import select_output_by
 from ._talib import indicators
 # from ._plotly import plot_ohlcv, iplot_ohlcv, AnalysisFigure
-# from ._backtest_analysis import get_backtest, get_latest_backtest_info
+from ._backtest_analysis import get_backtest, get_latest_backtest_info
 
 __all__ = (
     'run_pipeline',
@@ -16,7 +16,7 @@ __all__ = (
     'to_tdates',
     # 'ohlcv',
     'indicators',
-    # 'get_backtest',
-    # 'get_latest_backtest_info',
+    'get_backtest',
+    'get_latest_backtest_info',
     'get_pricing',
 )
