b175fb0d579e649cb4b76400ad95db6ff4171e24
liudengfeng <liu.dengfeng@hotmail.com>
2019-07-16 18:21:56 +0800

    使用后复权价(不会因分红派息而调整)

--- zipline/pipeline/factors/basic.py
+++ zipline/pipeline/factors/basic.py
@@ -59,7 +59,7 @@ class OpenReturns(CustomFactor):
     Calculate the opening change rate based on the previous closing price
     **Default Inputs**: [CNEquityPricing.close, CNEquityPricing.open]
     """
-    inputs = [CNEquityPricing.close, CNEquityPricing.open]
+    inputs = [CNEquityPricing.b_close, CNEquityPricing.b_open]
     window_safe = True
     window_length = 2
 
