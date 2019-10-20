3a7d1e8d813009dc391d987f7ffd5cf28ab20df6
ldf <liu.dengfeng@hotmail.com>
2019-10-20 10:17:46 +0800

    重构总市值、流通市值因子

--- zipline/optimize/factors/style.py
+++ zipline/optimize/factors/style.py
@@ -19,20 +19,20 @@ class Momentum(CustomFactor):
 
 
 class Size(CustomFactor):
-    inputs = (CNEquityPricing.market_cap,)
+    inputs = (CNEquityPricing.close, Fundamentals.equity.流通股本)
     window_length = 1
 
-    def compute(self, today, assets, out, market_cap):
-        out[:] = np.log(market_cap[-1])
+    def compute(self, today, assets, out, closes, mkt_equity):
+        out[:] = np.log(closes[-1] * mkt_equity[-1])
 
 
 class Value(CustomFactor):
-    inputs = (CNEquityPricing.market_cap,
+    inputs = (CNEquityPricing.close, Fundamentals.equity.流通股本,
               Fundamentals.balance_sheet.所有者权益或股东权益合计)
     window_length = 1
 
-    def compute(self, today, assets, out, market_cap, book_value):
-        out[:] = market_cap[-1] / book_value[-1]
+    def compute(self, today, assets, out, closes, mkt_equity, book_value):
+        out[:] = closes[-1] * mkt_equity[-1] / book_value[-1]
 
 
 def STR():
@@ -42,7 +42,7 @@ def STR():
 class Volatility(CustomFactor):
     """收益率波动"""
     inputs = (CNEquityPricing.close,)
-    window_length = 122 # 半年
+    window_length = 122  # 半年
 
     def compute(self, today, assets, out, closes):
         rets = np.diff(closes, axis=0) / closes[:-1]
