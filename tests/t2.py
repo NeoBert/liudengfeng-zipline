055c3b177ac1589f47e9ff1aa0537a29c32a3f01
ldf <liudengfeng_sd@outlook.com>
2020-05-03 00:21:48 +0800

    Scott Sanderson MAINT: Remove unused imports.

--- tests/t2.py
+++
@@ -1,6 +0,0 @@
-from trading_calendars import get_calendar
-from zipline.research.pipebench import _pipeline_engine_and_calendar_for_bundle
-from zipline.pipeline import Pipeline
-bundle = 'cndaily'
-engine, calendar = _pipeline_engine_and_calendar_for_bundle(bundle)
-p = Pipeline()
