f116a9379ae82ec1d61c1e7bbfe002a2cae69d09
LDF <liudengfeng_sd@outlook.com>
2020-07-02 23:27:30 +0800

    修改国债利率读取方式

--- zipline/data/treasuries_cn.py
+++ zipline/data/treasuries_cn.py
@@ -5,7 +5,8 @@ import pandas as pd
 
 from cnswd.utils import sanitize_dates
 from cnswd.websource.treasuries import EARLIEST_POSSIBLE_DATE
-from cnswd.store import TreasuryDateStore
+# from cnswd.store import TreasuryDateStore
+from cnswd.mongodb import get_db
 
 
 TREASURY_COL_MAPS = {
@@ -48,17 +49,27 @@ def get_treasury_data(start, end):
     Example:
     >>> start, end = '2020-03-10', '2020-03-15'
     >>> get_treasury_data(start, end).iloc[:3,:5]
-        cash	1month	2month	3month	6month
-    date					
-    2020-03-10 00:00:00+00:00	0.016000	0.016231	0.016610	0.016661	0.016991
-    2020-03-11 00:00:00+00:00	0.016000	0.016727	0.016996	0.017001	0.017211
-    2020-03-12 00:00:00+00:00	0.015742	0.016195	0.016993	0.016994	0.017625
+                                cash    1month    2month    3month    6month
+    date
+    2020-03-10 00:00:00+00:00  0.016000  0.016231  0.016610  0.016661  0.016991 
+    2020-03-11 00:00:00+00:00  0.016000  0.016727  0.016996  0.017001  0.017211 
+    2020-03-12 00:00:00+00:00  0.015742  0.016195  0.016993  0.016994  0.017625 
+    2020-03-13 00:00:00+00:00  0.014287  0.016395  0.016699  0.016705  0.017953
     """
-    with TreasuryDateStore() as store:
-        df = store.query(start, end)
+    start, end = sanitize_dates(start, end)
+    db = get_db()
+    collection = db['国债利率']
+    predicate = {
+        'date': {"$gte": start, "$lte": end}
+    }
+    projection = {"_id": 0}
+    sort = [("日期", 1)]
+    df = pd.DataFrame.from_records(
+        collection.find(predicate, projection, sort=sort))
+    # df.set_index('date', inplace=True)
+    df.index = pd.DatetimeIndex(df.pop('date'))
     # 缺少2年数据，使用简单平均插值
     value = (df['y1'] + df['y3']) / 2
     df.insert(7, '2year', value)
     df.rename(columns=TREASURY_COL_MAPS, inplace=True)
-    # df.index = pd.DatetimeIndex(df.index)
     return df.tz_localize('UTC')
